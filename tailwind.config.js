1822bd9cd9c55f1e25ad755c4a1d85b2dd92283e
Inventory Management Software <346ronokarya@gmail.com>
2024-09-30 01:56:54 +0600

    Landing page 15% complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        merriweather: ['Merriweather', 'serif'],
+      },
+      colors: {
+        header: '#262626', 
+        buyNow: '#82b440',
+        buyNowHover: '#7AA93C',
+        greenBody: '#034833',
+        playButtonZoom: '#046b4b',
+        getAnAppointment: '#76c644',
+        button: '#83CD20',
+      },
+    },
   },
   plugins: [],
 }
